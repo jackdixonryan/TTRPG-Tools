984064bf0f22f1dfc4cebea988f2fc959e6bce12
Jack Ryan <jack.dixon.ryan@gmail.com>
2021-07-22 23:55:35 -0600

    Init.

---
+++ rune-consumer/inventory.module.ts
@@ -0,0 +1,48 @@
+import { Item } from "./types";
+import { v4 } from "uuid";
+
+class Inventory {
+  constructor() {
+  };
+
+  inventoryContents: Item[] = [];
+
+  // adds a new item to the inventory.
+  add(item: Item): Item {
+    this.inventoryContents.push(item);
+    return item;
+  }
+
+  // gets an item from the inventory.
+  get(itemId: string): Item[] | null {
+    const matches: Item[] = this.inventoryContents.filter((item: Item) => item.itemId === itemId);
+    if (matches.length === 0) {
+      return null;
+    } else {
+      return matches;
+    }
+  }
+
+  show(): void {
+    console.log(this.inventoryContents);
+  }
+
+  // takes an item out of inventory.
+  remove(itemId: string): Item[] {
+    const inventoryWithoutItem: Item[] = this.inventoryContents.filter((item: Item) => item.itemId !== itemId);
+    this.inventoryContents = inventoryWithoutItem;
+    return inventoryWithoutItem;
+  }
+  
+}
+
+const id = v4();
+const inventory = new Inventory();
+inventory.add({
+  itemId: id,
+  itemName: "Fire Rune"
+});
+const item = inventory.get(id);
+inventory.remove(id)
+inventory.show();
+console.log(item);
